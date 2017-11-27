520809f31870c11d6cd20f78ba89bbc585d304ae
Matt Cooper <matthew_cooper@brown.edu>
2017-11-27 18:31:05 -0500

    Fixed syntax errors in run

--- run.py
+++ run.py
@@ -36,8 +36,8 @@ def run(args):
 
     sess = tf.Session()
     # TODO: Switch to ACKTR model
-    # agent = ACKTRModel(sess, args, env.action_space.n)
-    agent = RandomAgent(sess, args, env.action_space.n)
+    agent = ACKTRModel(sess, args, env.action_space.n)
+    # agent = RandomAgent(sess, args, env.action_space.n)
     sess.run(tf.global_variables_initializer())
 
     global_step = 0
@@ -53,21 +53,19 @@ def run(args):
 
         while True:
             # Fill up the batch until it is full or we reach a terminal state
-            if len(batch['action']) < c.BATCH_SZ && !terminal:
+            if len(batch['action']) < args.batch_size and not terminal:
                 start_state = state
                 action = agent.get_action(np.expand_dims(state, axis=0))
                 state, reward, terminal, _ = env.step(action)
 
                 # The SARS queue is full so the first item will be popped off
-                if (len(buff) == args.k):
+                if len(buff) == args.k:
                     popped_sars = buff[0]
 
                     # Compute the discounted reward
                     r_d = 0
-                    discount = 1
                     for i in range(args.k):
-                        r_d = popped_sars[2] * discount
-                        discount *= args.gamma
+                        r_d += buff[i][2] * args.gamma**i
 
                     # Add the SARS to the batch
                     add_sars_to_batch(popped_sars, r_d)
@@ -82,11 +80,11 @@ def run(args):
                     states = np.array(batch['state'])
                     actions = np.array(batch['action'])
                     rewards = np.array(batch['reward'])
-                    terminas = np.array(batch['terminal'])
+                    terminals = np.array(batch['terminal'])
 
                     # TODO 1. check on the shape of states
                     # TODO 2. do we need next states somewhere (might be related to 1.)
-                    global_step = agent.train_step(np.expand_dims(states, axis=0),
+                    global_step = agent.train_step(states,
                                                    actions,
                                                    rewards,
                                                    terminals)
