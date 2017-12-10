a56e71a231e8afc4cef9af07539808d4eb7a9c54
Michael Gillett <mgillett54@gmail.com>
2017-12-09 22:53:05 -0500

    Switched to softmax for experimenting

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ class Runner:
         num_steps = self.args.batch_size // self.args.num_envs
         for step_num in xrange(num_steps):
             # Pick an action and perform it in the envs
-            actions = self.agent.get_actions(self.states)
+            actions = self.agent.get_actions_softmax(self.states)
             next_states, rewards, terminals, infos = self.env.step(actions)
 
             # TODO: why only saving ep reward for 0th env?
