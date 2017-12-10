f002e8465bb8ba4e1d5ddba619db7a8324e7a097
Matt Cooper <matthew_cooper@brown.edu>
2017-12-09 22:15:22 -0600

    get_actions_softmax working

--- acktr_model.py
+++ acktr_model.py
@@ -194,8 +194,8 @@ class ACKTRModel:
         feed_dict = {self.x_batch: states}
         policy_probs = self.sess.run(self.policy_probs, feed_dict=feed_dict)
         # policy_probs = np.squeeze(policy_probs)
-        actions = [np.random.choice(len(state_probs), p=policy_probs) for state_probs in policy_probs]
-        return
+        actions = np.array([np.random.choice(len(state_probs), p=state_probs) for state_probs in policy_probs])
+        return actions
 
     def get_actions(self, states):
         feed_dict = {self.x_batch: states}
