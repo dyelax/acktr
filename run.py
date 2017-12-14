44beb6c7389eff9a9850d7aacebadaaa6b5af201
Michael Gillett <mgillett54@gmail.com>
2017-12-14 15:07:06 -0500

    Converted batching code to use real k-step

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from utils import get_env, parse_args, transform_monitor, show_state
 from random_agent import RandomAgent
 from acktr_model import ACKTRModel
 import constants as c
+from collections import deque
 
 
 class Runner:
@@ -25,15 +26,20 @@ class Runner:
         self.terminals = np.repeat([False], self.args.num_envs)
         self.global_step = 0
 
-    def get_batch(self):
-        batch_states = []
-        batch_actions = []
-        batch_rewards = []
-        batch_terminals = []
+        # The buffer size is 2 * num_steps
+        self.num_steps = self.args.batch_size // self.args.num_envs
+        buff_size = self.num_steps * 2
+        self.batch_states_buff = deque(maxlen=buff_size)
+        self.batch_actions_buff = deque(maxlen=buff_size)
+        self.batch_rewards_buff = deque(maxlen=buff_size)
+        self.batch_terminals_buff = deque(maxlen=buff_size)
 
+        # Prefill the buffer with 20 steps
+        self.collect_steps()
+
+    def collect_steps(self):
         # Take the number of steps across all envs to fill a batch
-        num_steps = self.args.batch_size // self.args.num_envs
-        for step_num in xrange(num_steps):
+        for step_num in xrange(self.num_steps):
             # Pick an action and perform it in the envs
             actions = self.agent.get_actions(self.states)
 
@@ -42,13 +48,13 @@ class Runner:
             # everything should be relative to the state (state was terminal and we took action
             # from it and that lead to getting reward).
             # TODO: These copies might not be necessary.
-            batch_states.append(np.copy(self.states))
-            batch_terminals.append(np.copy(self.terminals))
-            batch_actions.append(actions)
+            self.batch_states_buff.append(np.copy(self.states))
+            self.batch_terminals_buff.append(np.copy(self.terminals))
+            self.batch_actions_buff.append(actions)
 
             self.states, rewards, self.terminals, infos = self.env.step(actions)
 
-            batch_rewards.append(rewards)
+            self.batch_rewards_buff.append(rewards)
 
             # This will trigger when the 0th env has finished a full episode.
             if infos[0]['real_done']:
@@ -61,55 +67,55 @@ class Runner:
 
                 self.agent.write_ep_reward_summary(infos[0]['ep_reward'], infos[0]['env_steps'])
 
-        batch_terminals.append(self.terminals)
+        self.batch_terminals_buff.append(self.terminals)
+
+    def get_batch(self):
+        # Collect an additional 20 env_steps
+        self.collect_steps()
 
         # Flipping from num_steps x num_envs to num_envs x num_steps
-        #  (20 x 32 to 32 x 20)
-        batch_states = np.array(batch_states).swapaxes(1, 0)
-        batch_actions = np.array(batch_actions).swapaxes(1, 0)
-        batch_rewards = np.array(batch_rewards).swapaxes(1, 0)
-        batch_terminals = np.array(batch_terminals).swapaxes(1, 0)
+        #  (40 x 32 to 32 x 40)
+        batch_states = np.array(self.batch_states_buff).swapaxes(1, 0)
+        batch_actions = np.array(self.batch_actions_buff).swapaxes(1, 0)
+        batch_rewards = np.array(self.batch_rewards_buff).swapaxes(1, 0)
+        batch_terminals = np.array(self.batch_terminals_buff).swapaxes(1, 0)
 
         batch_terminals = batch_terminals[:, 1:]
 
-        # Compute the discounted reward
-        # NOTE: the discounted reward is computed over the num_steps
-        #       rewards earlier get more "look ahead" reward added
-        #       to them than later states
-        # The value of the next_state for each env
-        values_of_next_states = self.agent.get_values(self.states)
-        # Loop over envs
+        # Loop over envs and Compute the discounted reward
         for i, (env_rewards, env_terminals) in enumerate(zip(batch_rewards, batch_terminals)):
-            # Append value of next state to the rewards if episode didn't end on the last step
-            env_last_terminal = batch_terminals[i, -1]
-            if not env_last_terminal:
-                v_s_next = values_of_next_states[i]
-                env_rewards = np.append(env_rewards, v_s_next)
-                env_terminals = np.append(env_terminals, 0)
+            # The value of the next_states for each step in the current env
+            values_of_next_states = self.agent.get_values(batch_states[self.num_steps:])
+            discounted_rewards = []
 
-            new_rewards = []
-            # TODO: They don't stop when they hit a terminal, but maybe we should
-            # building up new_rewards in reverse order
-            discounted_future_reward = 0
-            for j in reversed(xrange(len(env_rewards))):
-                reward = env_rewards[j]
-                terminal = env_terminals[j]
-                discounted_future_reward *= self.args.gamma * (1 - terminal)
-                discounted_future_reward += reward
-                new_rewards.append(discounted_future_reward)
+            # For each step in the env
+            for step in self.num_steps:
+                r_d = 0
+                discount = 1
 
-            # built new_rewards up in reverse order, so now put it back in time order
-            new_rewards.reverse()
+                # Discount over the 20 next states
+                for j in self.num_steps:
+                    reward = env_rewards[step + j]
+                    terminal = env_terminals[step + j]
 
-            # removing extra entry for value of next state (when not terminal)
-            if not env_last_terminal:
-                batch_rewards[i, :] = np.array(new_rewards[:-1])
-                batch_terminals[i, :] = env_terminals[:-1]
+                    if terminal: # Stop discounting when we reach a terminal
+                        r_d += reward
+                        break
+                    else:
+                        r_d += reward * discount
+
+                    discount *= self.args.gamma
+
+                # Add value of next state if the last state num steps ahead is not terminal
+                if not batch_terminals[j + self.num_steps] and j == self.num_steps - 1:
+                    r_d += values_of_next_states[step]
+
+                discounted_rewards.append(r_d)
 
         # Reshape into (batch_size,) + element.shape
-        batch_states = batch_states.reshape((-1, c.IN_HEIGHT, c.IN_WIDTH, c.IN_CHANNELS))
-        batch_actions = batch_actions.flatten()
-        batch_rewards = batch_rewards.flatten()
+        batch_states = batch_states[:self.num_steps].reshape((-1, c.IN_HEIGHT, c.IN_WIDTH, c.IN_CHANNELS))
+        batch_actions = batch_actions[:self.num_steps].flatten()
+        batch_rewards = np.array(discounted_rewards).flatten() # discounted_rewards is already 20 long
 
         return batch_states, batch_actions, batch_rewards
 
